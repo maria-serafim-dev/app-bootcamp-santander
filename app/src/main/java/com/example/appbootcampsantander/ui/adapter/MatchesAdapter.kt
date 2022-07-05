f74bb920bb2c09ad4ca58ca338f64aa848f89977
maria-serafim-dev <maria.serafim.dev@gmail.com>
2022-07-04 21:06:06 -0300

    Utilizando Glide para o carregamento de imagens

--- app/src/main/java/com/example/appbootcampsantander/ui/adapter/MatchesAdapter.kt
+++ app/src/main/java/com/example/appbootcampsantander/ui/adapter/MatchesAdapter.kt
@@ -3,6 +3,7 @@ package com.example.appbootcampsantander.ui.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.appbootcampsantander.databinding.MatchItemBinding
 import com.example.appbootcampsantander.domain.Match
 
@@ -22,7 +23,11 @@ class MatchesAdapter(private val matches: List<Match>) : RecyclerView.Adapter<Ma
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val match = matches[position]
+
+        Glide.with(holder.itemView.context).load(match.homeTeam.image).circleCrop().into(holder.binding.ivHomeTeam)
         holder.binding.tvHomeTeamName.text = match.homeTeam.name
+
+        Glide.with(holder.itemView.context).load(match.awayTeam.image).circleCrop().into(holder.binding.ivAwayTeam)
         holder.binding.tvAwayTeamName.text = match.awayTeam.name
 
     }
